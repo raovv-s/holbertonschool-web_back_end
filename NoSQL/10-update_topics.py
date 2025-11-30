4c6d764d234997755bce26ef28c203fe5ff44bb0
raovv-s <nadirmaharramov875@gmail.com>
2025-11-30 14:59:35 +0000

    30nov

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,6 +4,8 @@ import mongo
 
 
 def update_topics(mongo_collection, name, topics):
+    """update function"""
+    
     mongo_collection.update_many(
         {"name": name},
         {$set: {"topics": topics}}
